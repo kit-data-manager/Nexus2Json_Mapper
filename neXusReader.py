ad2d6cc85079af0a04f4417cc1ea0d1871fe62e1
gabinoumbe <oumbegabin@yahoo.fr>
2025-02-05 17:53:32 +0100

    print6 ValueError for no valide input NeXus file

--- neXusReader.py
+++ neXusReader.py
@@ -42,8 +42,8 @@ class NeXusReader:
             with h5py.File(self.file_path, 'r') as f:
                 self.all_metadata = self.extract_metadata(f)
         except Exception as e:
-            raise ValueError(f"Error reading Nexus file: {e}")
-            #logging.info(f"Error reading Nexus file: {e}")
+            #raise ValueError(f"Error reading Nexus file: {e}")
+            logging.info(f"Error reading Nexus file: {e}")
         return self.all_metadata
 
     def _process_zip_file(self):
